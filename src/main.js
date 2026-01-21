f6bb5079336712a5995358007774ab5cdfd7f1dc
amigo228 <amiguys9@gmail.com>
2026-01-21 15:51:56 +0100

    sday3 fix opt3 final

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { GameScene } from './scenes/GameScene.js';
-import {PreloadScene} from './scenes/preloadScene.js';
+import {PreloadScene} from './scenes/PreloadScene.js';
 const config = {
     type: Phaser.AUTO,
     title: 'Overlord Rising',
